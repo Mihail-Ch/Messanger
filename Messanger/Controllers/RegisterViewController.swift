0e3afc92e322bef4f6e39990ea66d24c2453b984
michail <mihail.chertov@gmail.com>
2024-02-01 19:15:37 +0300

    Wrap up getting started

---
+++ Messanger/Controllers/RegisterViewController.swift
@@ -0,0 +1,29 @@
+//
+//  RegisterViewController.swift
+//  Messanger
+//
+//  Created by Михаил Чертов on 01.02.2024.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
