ead9521c7c432073b3e47c4b5d33276acfcfd010
michail <mihail.chertov@gmail.com>
2024-02-15 23:24:00 +0300

    Handle auth session, add database

--- Messanger/Controllers/ConversationsViewController.swift
+++ Messanger/Controllers/ConversationsViewController.swift
@@ -6,14 +6,30 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class ConversationsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemCyan
+        view.backgroundColor = .systemRed
+        
+       // DatabaseManager.shared.insertUser(with: <#T##ChatAppUser#>)
     }
 
-
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        validateAuth()
+    }
+    
+    private func validateAuth() {
+        if FirebaseAuth.Auth.auth().currentUser == nil {
+            let vc = LoginViewController()
+            let nav = UINavigationController(rootViewController: vc)
+            nav.modalPresentationStyle = .fullScreen
+            present(nav, animated: false)
+        }
+    }
 }
 
